305792219336d3e40df660e02f22d094b92e58ee
InsZVA <inszva@inszvademac-mini.local>
2016-04-07 20:20:58 +0800

    Initial Commit

---
+++ books/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  books
+//
+//  Created by InsZVA on 16/4/7.
+//  Copyright © 2016年 InsZVA. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
